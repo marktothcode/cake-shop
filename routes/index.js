e50e4e2cdb19975bec7f252152047b1550e57e81
marktothcode <mark-toth@outlook.com>
2018-05-25 08:02:40 +0100

    Navbar active class issue sorted

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Home = require("../models/home-text");
+var path;
 
 router.get("/", function(req, res) {
 
@@ -9,26 +10,27 @@ router.get("/", function(req, res) {
             console.log(err);
         }
         else {
-            res.render("index", { Home: homeText });           
+            res.render("index", { Home: homeText, path:req.path });           
            
         }
     });
 });
 
 router.get("/about-us", function(req, res) {
-    res.render('about-us');
+    res.render('about-us', {path : req.path});
+    
 });
 
 router.get("/services", function(req, res) {
-    res.render('services');
+    res.render('services', {path : req.path});
 });
 
 router.get("/offers", function(req, res) {
-    res.render('offers');
+    res.render('offers', {path : req.path});
 });
 
 router.get("/contact-us", function(req, res) {
-    res.render('contact-us');
+    res.render('contact-us', {path : req.path} );
 });
 
 module.exports = router;
